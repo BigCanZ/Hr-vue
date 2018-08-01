91442be805ac3aacb7ea0dcbc209b9010096de6c
Lizhou <870267715@qq.com>
2018-08-02 03:17:27 +0800

    components

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ export default {
     for (let i in allData) {
       let resultData = allData[i].rows
       let resultDataName = allData[i].meta.columns
-      var resultJson = {}
+      var resultJson = []
       let nameArr = resultDataName.map((item) => {
         return item.name
       })
@@ -15,18 +15,23 @@ export default {
         for (let index in item) {
           outArr[nameArr[index]] = item[index]
         }
-        resultJson[item[0]] = outArr
+        resultJson[i] = outArr
       })
       allData[i].rows = resultJson
       allData[i].meta = sortArr
     }
     let formData = allData.result.rows
+    let selectdata = []
     for (var m in formData) {
       if (formData[m].fieldForm === '1' || formData[m].fieldForm === '2' || formData[m].fieldForm === '7') {
         let selectData = allData['templatePropertyValue_' + formData[m].fieldId]
-        formData[m].selectdata = selectData
+        selectdata.push(selectData)
+        formData[m].selectdata = selectdata
       }
     }
-    return formData
+    // console.log(data.blocks.result.rows)
+    console.log(formData)
+    data.blocks.result.rows = formData
+    return data
   }
 }
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,37 +1,53 @@
 <template>
   <div class="input-outer">
     <!-- 文本框 -->
-    <div class="input-box border-bottom">
-      <span class="input-title">一级负责人</span>
-      <input type="text" class="input-text"/>
-    </div>
-    <!-- 只读状态下的文本框用div代替 -->
-    <div class="input-box border-bottom">
-      <span class="input-title">一级负责人</span>
-      <div class="input-text">111</div>
-    </div>
-    <!-- 文本域 -->
-    <div class="textarea-box">
-      <span class="input-title">一级负责人</span>
-      <textarea class="input-textarea"></textarea>
-    </div>
-    <!-- 只读状态下的文本域用div代替 -->
-    <div class="textarea-box">
-      <span class="input-title">一级负责人</span>
-      <div class="input-textarea">111</div>
-    </div>
-    <div class="choose-box">
-      <span class="input-title">事件阅知人员</span>
-      <div class="choose-inner-box">
-        <input type="radio" name="c" class="input-choose" id="radio_01">
-        <label for="radio_01">哈哈哈</label>
-        <input type="radio" name="c" class="input-choose" id="radio_02">
-        <label for="radio_02">哈哈哈</label>
-        <input type="radio" name="c" class="input-choose" id="radio_03">
-        <label for="radio_03">哈哈哈</label>
-        <input type="radio" name="c" class="input-choose" id="radio_04">
-        <label for="radio_04">哈哈哈</label>
+    <div v-for="item in this.fromData" :key="item.fieldId">
+      <div class="input-box border-bottom" v-if="showInput(item.fieldForm)">
+        <span class="input-title">{{item.fieldName}}</span>
+        <input type="text" class="input-text"
+        :id="item.propertyId"
+        :value="showDeatil (item.fieldForm, item) !== false ? showDeatil (item.fieldForm, item): item.propertyValue"
+        v-if="showRead(item.ifReadonly)"/>
+        <!-- 只读状态下的文本框用div代替 -->
+        <div class="input-text"
+        v-if="showReadOnly(item.ifReadonly)"
+        :id="item.propertyId">{{showDeatil (item.fieldForm, item) !== false ? showDeatil (item.fieldForm, item): item.propertyValue}}</div>
       </div>
+      <!-- <div class="input-box border-bottom" v-if="showReadOnlyInput(item.fieldForm, item.ifReadonly)">
+        <span class="input-title">{{item.fieldName}}</span>
+        <div class="input-text" :id="item.propertyId">{{item.propertyValue}}</div>
+      </div> -->
+      <!-- 文本域 -->
+      <div class="textarea-box" v-if="showTextarea(item.fieldForm)">
+        <span class="input-title">{{item.fieldName}}</span>
+        <textarea class="input-textarea"
+        :id="item.propertyId"
+        :value="item.propertyValue"
+        v-if="showRead(item.ifReadonly)"></textarea>
+        <!-- 只读状态下的文本域用div代替 -->
+        <div class="input-textarea"
+        :id="item.propertyId"
+        :value="item.propertyValue"
+        v-if="showReadOnly(item.ifReadonly)">{{item.propertyValue}}</div>
+      </div>
+      <!-- 只读状态下的文本域用div代替 -->
+      <!-- <div class="textarea-box">
+        <span class="input-title">{{item.fieldName}}</span>
+       
+      </div> -->
+      <!-- <div class="choose-box" v-if="showInput(item.fieldForm)">
+        <span class="input-title">事件阅知人员</span>
+        <div class="choose-inner-box">
+          <input type="radio" name="c" class="input-choose" id="radio_01">
+          <label for="radio_01">哈哈哈</label>
+          <input type="radio" name="c" class="input-choose" id="radio_02">
+          <label for="radio_02">哈哈哈</label>
+          <input type="radio" name="c" class="input-choose" id="radio_03">
+          <label for="radio_03">哈哈哈</label>
+          <input type="radio" name="c" class="input-choose" id="radio_04">
+          <label for="radio_04">哈哈哈</label>
+        </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -42,19 +58,69 @@ export default {
   name: 'Home',
   data () {
     return {
-      fromData: ''
+      fromData: {}
     }
   },
   methods: {
     getInfo () {
       this.$fetch(this.serviceUrl + 'index.json', {}, api.listData)
         .then((response) => {
-          console.log(response)
-          // let fromData = response.blocks.result.rows
-          // let dataSort = response.blocks.result.meta
-          // console.log(fromData)
-          // console.log(dataSort)
+          this.fromData = response.blocks.result.rows
         })
+    },
+    showDeatil (form, itemlist) {
+      if (form === '1' || form === '2') {
+        // 当组件对应的是下拉框
+        if (itemlist.propertyValue !== '') {
+          let rowList = itemlist.selectdata.rows
+          console.log(rowList[itemlist.propertyValue])
+          rowList.forEach(item => {
+            if (item.selectValueId === itemlist.propertyValue) {
+              // console.log(item.selectValue)
+              // console.log(itemlist.propertyValue)
+              return item.selectValue
+            } else {
+              return false
+            }
+          })
+        } else {
+          return false
+        }
+      } else {
+        return false
+      }
+    },
+    showRead (read) {
+      // 显示可编辑
+      if (read === '0') {
+        return true
+      } else {
+        return false
+      }
+    },
+    showReadOnly (read) {
+      // 显示只读
+      if (read === '1') {
+        return true
+      } else {
+        return false
+      }
+    },
+    showInput (form) {
+      // 显示输入框
+      if (form === '0' || form === '1' || form === '2') {
+        return true
+      } else {
+        return false
+      }
+    },
+    showTextarea (form) {
+      // 显示文本域
+      if (form === '4' || form === '10') {
+        return true
+      } else {
+        return false
+      }
     }
   },
   created () {
